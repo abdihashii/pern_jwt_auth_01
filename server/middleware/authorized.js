5cb2f7167a269ab19f12fc4a58603d67d9cba8b5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2021-06-04 00:03:00 -0500

    fix: authorized middleware to catch empty tokens

--- server/middleware/authorized.js
+++ server/middleware/authorized.js
@@ -8,13 +8,13 @@ module.exports = async (req, res, next) => {
   } = req;
 
   // 2. if they don't have a token, then we throw an error
-  if (!token) {
+  if (token === null) {
     return res.status(403).json({ message: 'Not authorized! Please log in' }); // 403 status error is for unauthorized users
   }
 
   try {
     // 3. check if the jwt token is valid
-    const payload = await jwt.verify(token, process.env.jwtSecret);
+    const payload = jwt.verify(token, process.env.jwtSecret);
 
     // 4. if the token is valid, then we set the req user to the payload, so the routes using this middleware have access to it
     req.user = payload.user;
@@ -22,6 +22,6 @@ module.exports = async (req, res, next) => {
     next();
   } catch (error) {
     console.error(error.message);
-    return res.status(403).json({ message: 'Not authorized!' }); // 403 status error is for unauthorized users
+    res.status(401).json({ message: 'Token is not valid!' }); // 403 status error is for unauthorized users
   }
 };
