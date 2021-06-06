98e4da4fb5071ff4d19f06e8f0c778deeb2fdebe
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2021-06-06 01:42:18 -0500

    feat: adding redux toolkit to project

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 // Libraries
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from '../slices/counter/counterSlice';
 
 const Dashboard = ({ setAuth }) => {
   const [user, setUser] = useState({
     name: '',
     email: '',
   });
+  // Redux
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   const onLogOut = (e) => {
     e.preventDefault();
@@ -45,8 +50,37 @@ const Dashboard = ({ setAuth }) => {
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <div>Hello, {user.name}</div>
+      <h1 className="my-3">Dashboard</h1>
+      <div className="my-3">Hello, {user.name}</div>
+
+      <div className="my-3" style={{ width: 'max-content' }}>
+        <button
+          className="btn btn-outline-success"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span className="mx-3">{count}</span>
+        <button
+          className="btn btn-outline-danger"
+          aria-label="Increment value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button
+          className="btn btn-outline-primary d-block w-100 my-3"
+          aria-label="Reset value"
+          onClick={() => dispatch(reset())}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div className="my-3">
+        <UsersTable {...{ users }} />
+      </div>
 
       <button className="btn btn-primary btn-block" onClick={onLogOut}>
         Log out
