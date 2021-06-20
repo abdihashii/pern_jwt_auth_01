e11d83683c06e4aa55a5a1b180dc78916325a814
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2021-06-19 20:17:03 -0500

    refactor: update redux store to typescript

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,9 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../slices/counter/counterSlice';
 import authenticationReducer from '../slices/authentication/authenticationSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     counter: counterReducer,
     authenticationInputs: authenticationReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
